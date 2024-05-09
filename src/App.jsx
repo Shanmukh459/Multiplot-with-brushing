d19e6a1b2ce8b66cfe036cce26571419eac7a912
Shanmukh459 <saikrrish459@gmail.com>
2024-05-09 15:43:17 -0700

    world map visible

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,35 @@
+import { geoNaturalEarth1, geoPath, json } from "d3"
+import React, { useState, useEffect } from "react"
+import { feature } from "topojson"
+
+const width = 960
+const height = 500
+
+const projection = geoNaturalEarth1()
+const path = geoPath(projection)
+const jsonUrl = 'https://unpkg.com/world-atlas@2.0.2/countries-50m.json'
+
 function App() {
-  return <h1>Hello!</h1>
+  const [data, setData] = useState(null)
+
+  useEffect(() => {
+    json(jsonUrl).then(topology => {
+      const {countries} = topology.objects 
+      setData(feature(topology, countries))
+    })
+  }, [])
+
+  if (!data) {
+    return <h1>Loading...</h1>
+  }
+  return (
+    <svg height={height} width={width} >
+      {data.features.map((feature, i) => (
+        <path key={i} d={path(feature)} />
+      ))}
+
+    </svg>
+  )
 }
 
 export default App
